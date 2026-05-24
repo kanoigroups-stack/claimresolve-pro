4957a0e42d9c323dc6552d2c81512ec7489ff542
kanoigroups-stack <kanoigroups@gmail.com>
2026-05-24 16:49:07 +0530

    Format images config and add trailingSlash option

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  // Required for dynamic blog routes with static export
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
