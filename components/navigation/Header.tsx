f02196d07caef9f9d9e2992d60f2e24dcf8a733d
kanoigroups-stack <kanoigroups@gmail.com>
2026-05-27 20:42:14 +0530

    Update Header.tsx

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -24,6 +24,18 @@ export default function Header() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // Lock body scroll when mobile menu is open
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isMobileMenuOpen]);
+
   return (
     <>
       <motion.header
